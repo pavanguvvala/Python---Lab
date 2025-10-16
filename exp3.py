d0e8154a02d03730c663d55b388ce9f282354829
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-16 11:05:07 +0530

    Create exp3.py

---
+++ exp3.py
@@ -0,0 +1,7 @@
+# To write a Python program that uses a while loop to print the numbers: 8, 11, 14, 17, 	20, ….., 86, 89
+
+print("Using a while loop:")
+	num_while = 8
+	while num_while <= 89:
+    		print(num_while)
+    		num_while += 3
