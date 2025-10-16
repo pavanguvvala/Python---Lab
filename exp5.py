5145ccf6a7bdfc679a1c378822e313b760a2632b
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-16 11:07:06 +0530

    Create exp5.py

---
+++ exp5.py
@@ -0,0 +1,7 @@
+# To convert a given string into a list of its individual characters by iterating through the string using a for loop
+
+my_string = "exam"
+char_list = []
+for char in my_string:
+	char_list.append(char)
+print(char_list)
