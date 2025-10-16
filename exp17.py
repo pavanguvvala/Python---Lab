d6c4007c8773e595c5d1475d5dc436c2dcafd85e
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-16 11:31:18 +0530

    Create exp17.py

---
+++ exp17.py
@@ -0,0 +1,7 @@
+# To write a Pandas program to create and display a one-dimensional-array-like object containing an array of data using Pandas module
+
+import pandas as pd
+import numpy as np
+
+data_list = pd.Series([2,4,6,8,10])
+print(data_list)
