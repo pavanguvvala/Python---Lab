d13fe2a7e10fe975495ff6880cf4bc6aaf98fc0f
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-16 11:24:56 +0530

    Create exp12.py

---
+++ exp12.py
@@ -0,0 +1,9 @@
+# To write a NumPy program using methods – info, add, array, all, greater, greater_equal, less, less_equal, equal, allclose, zeroes, ones, linespace, tolist.
+# (a)  To get help on the add function
+
+import numpy as np
+
+# Get help on the numpy.add function
+help(np.add)
+# or in an interactive environment like Jupyter:
+# np.add?
