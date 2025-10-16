b520e7ef2e0dc2bec68225f1d1f3c61ccef1ab7b
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-16 11:03:05 +0530

    Create exp1.py

---
+++ exp1.py
@@ -0,0 +1,15 @@
+# To write a Python program that converts kilograms into pounds
+
+def kg_to_pounds():
+    """	
+    Converts a weight entered in kilograms to pounds.
+    """
+try:
+        kilograms = float(input("Enter the weight in kilograms: "))
+        pounds = kilograms * 2.20462
+        print(f"The weight in pounds is: {pounds:.2f}")
+except ValueError:
+        print("Invalid input. Please enter a numerical value for weight.")
+
+# Call the function directly to run the conversion
+kg_to_pounds()
